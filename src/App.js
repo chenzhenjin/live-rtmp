9044107bf78f30ce9653613618b40fdf743865d8
chenzhenjin <971561770@qq.com>
2020-11-01 22:22:01 +0800

    收工v1.0.0.5

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import * as Sentry from '@sentry/react';
 Sentry.init({
   dsn: "http://42d63c152d5648e3a680a700322a30ec@120.48.19.90:9000/3",
   logErrors:true,
-  release: 'v1.0.4'
+  release: 'v1.0.5'
 });
 function App() {
   return (
