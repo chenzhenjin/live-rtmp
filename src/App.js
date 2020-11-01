9df3f7d36056cb2996d222c89bec18aad9140626
chenzhenjin <971561770@qq.com>
2020-11-01 22:06:40 +0800

    对齐版本

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import * as Sentry from '@sentry/react';
 Sentry.init({
   dsn: "http://42d63c152d5648e3a680a700322a30ec@120.48.19.90:9000/3",
   logErrors:true,
-  release: 'pro@1.0.2'
+  release: 'v1.0.4'
 });
 function App() {
   return (
