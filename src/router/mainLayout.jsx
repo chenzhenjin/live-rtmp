88935e157d10905ce2f08777fcc00917287f8a3a
chenzhenjin <971561770@qq.com>
2020-11-08 19:17:06 +0800

    触发

--- src/router/mainLayout.jsx
+++ src/router/mainLayout.jsx
@@ -34,7 +34,7 @@ export default class MainLayout extends React.Component {
             <Link to="/state"
               onClick={() => { this.changeActive('state') }}
             >
-              <span>最新动态(未开发1.0.5版本号)</span>
+              <span>最新动态(1.0.5版本号)</span>
             </Link>
           </div>
         </div>
